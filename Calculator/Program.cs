35bca04ce0b379bc138c46b08ef4829b32942e6c
user <user@DESKTOP-JF02BG2>
2024-02-11 19:27:37 +0600

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1;
+            int num2;
+
+            string answer;
+
+            int result;
+
+            Console.WriteLine("Hello, welcome to the calculator program!");
+            
+            Console.WriteLine("Please enter your first number.");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please, enter your second number.");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What type of operation would you like to do?");
+
+            Console.WriteLine("Please, enter a for addition, s for substraction, m for multiplication or any other key for division.");
+            answer = Console.ReadLine();
+
+            if(answer == "a")
+            {
+                result = num1 + num2;
+            }
+
+            else if(answer == "s")
+            {
+                result = num1 - num2;
+            }
+
+            else if(answer == "m")
+            {
+                result = num1 * num2;
+            }
+
+            else
+            {
+                result = num1 / num2;
+            }
+
+            Console.WriteLine("The result is " + result);
+            Console.WriteLine("Thank you for using calculator program!");
+
+            Console.ReadKey();
+        }
+    }
+}
